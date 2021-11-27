7b1fe09cb42746166bf25ce61064515f3028b4d8
kurama8103 <38579582+kurama8103@users.noreply.github.com>
2021-11-27 11:11:31 +0900

    update

--- gpxdf/gpxdf.py
+++ gpxdf/gpxdf.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
+from datetime import timedelta
 import gpxpy
 import pandas as pd
 from geopy.distance import distance
@@ -7,6 +8,7 @@ import xml.etree.ElementTree as ET
 from xml.dom import minidom
 import folium
 import fitparse
+import os
 
 
 def read_gpx(filename):
@@ -35,17 +37,12 @@ def read_gpx(filename):
 
 def add_velocity(df_gpx):
     df_t = pd.DataFrame()
-
     # time difference
     df_t['diff_time'] = df_gpx.time.diff()
-
     # distance
     df_t['distance'] = distance_df(df_gpx[['latitude', 'longitude']])
-
     # velocity
-    df_t['velocity'] = df_t['distance'] / \
-        df_t.diff_time.apply(lambda x: x.seconds)*3.6
-
+    df_t['velocity'] = df_t['distance'] / df_t['diff_time'].dt.seconds*3.6
     return pd.concat([df_gpx, df_t], axis=1)
 
 
@@ -63,6 +60,16 @@ def to_gpx(df, filename=None):
 
     trk_n = None
     seg_no = None
+
+    try:
+        df.trackname
+    except:
+        df['trackname'] = None
+    try:
+        df.segment_no
+    except:
+        df['segment_no'] = None
+
     for i, row in df.iterrows():
         # track
         if row.trackname != trk_n:
@@ -100,12 +107,13 @@ def to_gpx(df, filename=None):
         )
 
 
-def to_html_map(df, filename, zoom_start=6):
+def to_html_map(df, filename='map.html', zoom_start=6):
     df_t = df[['latitude', 'longitude']]
-    map = folium.Map(
+    map_ = folium.Map(
         location=(df_t.iloc[0]+df_t.iloc[-1])/2, zoom_start=zoom_start)
-    folium.PolyLine(df_t).add_to(map)
-    map.save(filename)
+    [folium.PolyLine(df[['latitude', 'longitude']]).add_to(map_)
+     for t, df in df.dropna().groupby('trackname')]
+    map_.save(filename)
 
 
 def read_fit(filename):
@@ -118,17 +126,47 @@ def read_fit(filename):
         for data in record:
             d[data.name] = data.value
         list_.append(d)
-    return pd.DataFrame(list_)
+    df_ = pd.DataFrame(list_)
+    df_['trackname'] = os.path.splitext(filename)[0]
+    return df_
 
 
-def fit_to_gpx(df_fit):
+def fit_to_gpx_df(df_fit):
     df = pd.DataFrame()
-    df['latitude'] = df_fit.position_lat*180/(2**31)
-    df['longitude'] = df_fit.position_long*180/(2**31)
-    df['elevation'] = df_fit.enhanced_altitude
-    df['time'] = df_fit.timestamp  # UTC
-    df['diff_time'] = df_fit.timestamp.diff()
+    df['latitude'] = df_fit['position_lat']*180/(2**31)
+    df['longitude'] = df_fit['position_long']*180/(2**31)
+    df['elevation'] = df_fit['enhanced_altitude']
+    df['time'] = df_fit['timestamp']  # UTC
+    df['trackname'] = df_fit['trackname']
+    df['diff_time'] = df_fit['timestamp'].diff()
     df['distance'] = distance_df(df[['latitude', 'longitude']])
-    df['velocity'] = df['distance'] / \
-        df['diff_time'].apply(lambda x: x.seconds)*3.6
+    df['velocity'] = df['distance'] / df['diff_time'].dt.seconds*3.6
     return df
+
+
+def gpx_sammary(df, cut_velocity=1):
+    dic_ = {}
+    dic_['trackname'] = df['trackname'][0]
+    dic_['total_distance'] = df['distance'].sum()/1000
+    dic_['moving_time'] = df.query(
+        'velocity>=@cut_velocity')['diff_time'].sum()
+    dic_['moving_hours'] = dic_['moving_time']/timedelta(hours=1)
+    dic_['avg_velocity'] = dic_['total_distance'] / dic_['moving_hours']
+    return dic_
+
+
+class GPSDF:
+    def __init__(self, path):
+        self.ext = os.path.splitext(path)[1]
+        if self.ext == '.gpx':
+            self.df_raw = read_gpx(path)
+            self.df = add_velocity(self.df_raw)
+        elif self.ext == '.fit':
+            self.df_raw = read_fit(path)
+            self.df = fit_to_gpx_df(self.df_raw)
+
+    def to_gpx(self, filename=None):
+        return to_gpx(self.df, filename)
+
+    def to_html_map(self, filename='map.html', zoom_start=8):
+        return to_html_map(self.df, filename, zoom_start)
