67e682a799bc7e1375b73ce17fea93aae8631cad
kurama8103 <38579582+kurama8103@users.noreply.github.com>
2021-11-27 11:18:06 +0900

    revise

--- gpxdf/__init__.py
+++ gpxdf/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 # coding: utf-8
-from .gpxdf import read_gpx, add_velocity, distance_df, to_gpx, to_html_map, read_fit, fit_to_gpx
+from .gpxdf import read_gpx, add_velocity, distance_df, to_gpx, to_html_map, read_fit, fit_to_gpx_df, gpx_sammary, GPSDF
